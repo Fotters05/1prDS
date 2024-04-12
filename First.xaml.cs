dcdf61daf623b2764c1a1571716ad4882cb425e1
Fotters05 <t50_n.n.kudrov@mpt.ru>
2024-04-13 00:11:56 +0300

    1prDS

---
+++ First.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Praktika1_DataSet.praktika1_datasetDataSetTableAdapters;
+
+namespace Praktika1_DataSet
+{
+    public partial class First : Page
+    {
+        SUSHIBARSTableAdapter SUSHIBARS = new SUSHIBARSTableAdapter();
+
+        public First()
+        {
+            InitializeComponent();
+            SushiDataGrid.ItemsSource = SUSHIBARS.GetData();
+        }
+        private void ShowMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
+        }
+    }
+}
---
+++ Fourth.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Praktika1_DataSet.praktika1_datasetDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktika1_DataSet
+{
+    public partial class Fourth : Page
+    {
+        INFOBARSTableAdapter INFOBARS = new INFOBARSTableAdapter();
+
+
+        public Fourth()
+        {
+            InitializeComponent();
+            SushiDataGrid.ItemsSource = INFOBARS.GetData();
+        }
+
+        private void ShowMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
+        }
+    }
+}
